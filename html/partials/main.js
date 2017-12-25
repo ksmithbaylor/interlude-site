c50beae63518ad0bb04810dc2278318cf38a3ce0
Kevin Smith <ksmithbaylor@gmail.com>
2017-12-25 17:15:28 -0500

    Add JS with nanoajax for eventual pushState functionality

---
+++ html/partials/main.js
@@ -0,0 +1,13 @@
+(function (window) {
+  var links = ;
+  document.querySelectorAll('nav li a').forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      // event.preventDefault();
+      onClickNav(this.parentNode.classList[0]);
+    });
+  });
+
+  function onClickNav(target) {
+
+  }
+})(window);
